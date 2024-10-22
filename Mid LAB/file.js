125bad73f94c2f048e4664cbbe7c09feb76f573b
Niaz-Iqbal <niaziqbal1357@gmail.com>
2024-10-22 16:06:02 +0500

    ..

--- Mid LAB/file.js
+++ Mid LAB/file.js
@@ -1,13 +1,1 @@
 
-const profilePicture = document.querySelector('.profile-picture img');
-const introBox = document.querySelector('.intro-box');
-
-
-profilePicture.addEventListener('mouseenter', function() {
-    introBox.style.display = 'block';  
-});
-
-
-profilePicture.addEventListener('mouseleave', function() {
-    introBox.style.display = 'none';   
-});
